986f270f7157f4abd4779dfaa1ab23ac76a80d5a
kyslegion <kyslegion@gmail.com>
2023-04-30 11:42:16 +0200

    -correctif mineur

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const compression = require('compression');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // Utilisez le middleware de compression
 app.use(compression());
