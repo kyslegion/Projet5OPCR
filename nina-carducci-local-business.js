eda2f54295d84bcb4da9e2101f2e8db46103c225
kyslegion <kyslegion@gmail.com>
2023-05-01 18:46:50 +0200

    style.css: -correction problème format d'image
    index.html:
    -Déplacemement de schéma de structure ld json
    
    nina-carducci-local-business.js:
    -Ajout du fichier pour contenir la structure ld json

---
+++ nina-carducci-local-business.js
@@ -0,0 +1,22 @@
+{
+    "@context": "http://schema.org",
+    "@type": "LocalBusiness",
+    "name": "Nina Carducci",
+    "image": "http://www.googleusercontent.com/assets/images/nina.jpg",
+    "telephone": "05 56 67 78 89",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "68 avenue Alsace-Lorraine",
+      "addressLocality": "Bordeaux",
+      "postalCode": "33200"
+    },
+    "openingHoursSpecification": {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": {
+        "@type": "DayOfWeek",
+        "name": "lundi au vendredi"
+      },
+      "opens": "2023-04-30T10:00:00+02:00",
+      "closes": "2023-04-30T19:00:00+02:00"
+    }
+  }
